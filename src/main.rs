8d0c12a2106b19e2f17c5a8aa735588395015f3a
Leandro Serra <leandro.serra@lambdaclass.com>
2024-11-14 09:29:58 -0300

    Add logic to draw the cells on screen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
+mod conways;
+
+use conways::ConwaysGrid;
 use macroquad::prelude::*;
 
-#[macroquad::main("conways-rust")]
-async fn main() {
-    loop {
-        clear_background(RED);
+const CELL_SIZE: f32 = 20_f32;
 
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+fn conf() -> Conf {
+    Conf {
+        window_title: String::from("Conway's Game of Life"),
+        window_width: 640,
+        window_height: 640,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(conf)]
+async fn main() {
+    let conways = ConwaysGrid::default();
+    loop {
+        clear_background(WHITE);
+        for (row_num, row) in conways.grid.iter().enumerate() {
+            for (col_num, col) in row.iter().enumerate() {
+                draw_rectangle(
+                    row_num as f32 * CELL_SIZE,
+                    col_num as f32 * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE,
+                    match col {
+                        conways::CellState::Alive => WHITE,
+                        conways::CellState::Dead => BLACK,
+                    },
+                );
+            }
+        }
 
         next_frame().await
     }
