33fa2620f33e12d027aa99fa226e4c503f0d6117
Leandro Serra <leandro.serra@lambdaclass.com>
2024-11-14 15:58:24 -0300

    Add some initial configurations and the logic to switch between them

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod conways;
 
-use conways::ConwaysGrid;
+use conways::{ConwaysGrid, Position};
 use macroquad::prelude::*;
 
 const CELL_SIZE: f32 = 20_f32;
 const UPDATE_TIMER: f64 = 0.1;
 const PRESS_START_TEXT: &str = "Press space to start";
-const CONTROLS_TEXT: &str = "Press 0-9 in game for different configurations";
+const CONTROLS_TEXT: &str = "Press 1-5 in game for different configurations";
 const SCREEN_WIDTH: i32 = 640;
 const SCREEN_HEIGHT: i32 = 640;
 
@@ -22,27 +22,88 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
-    let alive_cells = vec![
-        (1, 3),
-        (1, 4),
-        (2, 1),
-        (2, 6),
-        (3, 7),
-        (4, 1),
-        (4, 7),
-        (5, 2),
-        (5, 3),
-        (5, 4),
-        (5, 5),
-        (5, 6),
-        (5, 7),
+    let configurations: [Vec<Position>; 5] = [
+        vec![
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (2, 3),
+            (3, 2),
+            (6, 6),
+            (6, 7),
+            (7, 6),
+            (7, 8),
+            (8, 7),
+            (11, 11),
+            (11, 12),
+            (12, 11),
+            (12, 13),
+            (13, 12),
+            (16, 16),
+            (16, 17),
+            (17, 16),
+            (17, 18),
+            (18, 17),
+            (21, 21),
+            (21, 22),
+            (22, 21),
+            (22, 23),
+            (23, 22),
+            (26, 26),
+            (26, 27),
+            (27, 26),
+            (27, 28),
+            (28, 27),
+        ],
+        vec![
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (2, 2),
+            (16, 16),
+            (16, 17),
+            (17, 16),
+            (17, 17),
+            (5, 5),
+            (5, 6),
+            (5, 7),
+        ],
+        vec![(1, 3), (2, 1), (2, 4), (3, 1), (3, 4), (4, 1)],
+        vec![(1, 3), (2, 1), (2, 3), (3, 2), (3, 3)],
+        vec![
+            (3, 5),
+            (3, 6),
+            (3, 7),
+            (5, 3),
+            (5, 9),
+            (6, 3),
+            (6, 9),
+            (7, 3),
+            (7, 9),
+            (9, 5),
+            (9, 6),
+            (9, 7),
+        ],
     ];
-    let mut conways = ConwaysGrid::from_alive_cells(alive_cells);
+    let mut index: usize = 0;
+    let mut conways = ConwaysGrid::from_alive_cells(configurations[index].clone());
     let mut last_updated = 0_f64;
     let mut game_started = false;
     loop {
         clear_background(WHITE);
         if game_started {
+            if let Some(last_keypress) = get_last_key_pressed() {
+                match last_keypress {
+                    KeyCode::Key1 => index = 0,
+                    KeyCode::Key2 => index = 1,
+                    KeyCode::Key3 => index = 2,
+                    KeyCode::Key4 => index = 3,
+                    KeyCode::Key5 => index = 4,
+                    _ => {}
+                }
+                conways = ConwaysGrid::from_alive_cells(configurations[index].clone());
+            }
+
             for (row_num, row) in conways.grid.iter().enumerate() {
                 for (col_num, col) in row.iter().enumerate() {
                     draw_rectangle(
